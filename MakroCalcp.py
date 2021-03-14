12a237ac5a597dfb03b179aa1bc8755b41dbcd3a
gast-andrzej <71961055+gast-andrzej@users.noreply.github.com>
2021-03-14 10:19:19 +0100

    Update MakroCalcp.py

--- MakroCalcp.py
+++ MakroCalcp.py
@@ -1,73 +1,86 @@
 """Macroscale->Usable Easy Program"""
+
 def makroCalcpr(func):
-    def ifif(*args):
-        if func(*args)<=1000:
-            def mk1k(*args):
+    
+    def ifif(*args,):
+        
+        if func(*args,)<=1000:
+            def mk1k(*args,):
                 y=1000
                 x=func(*args)
                 func(*args)
                 return print(f'{round(((x/y)*100), 5)}% from 1 000')
             return mk1k()
-        elif func(*args)>1000 and func(*args)<=10000:
-            def mk1k(*args):
+        
+        elif func(*args,)>1000 and func(*args)<=10000:
+            def mk1k(*args,):
                 y=10000
                 x=func(*args)
                 func(*args)
                 return print(f'{round(((x/y)*100), 5)}% from 10 000')
             return mk1k()
-        elif func(*args)>10000 and func(*args)<=100000:
-            def mk1k(*args):
+        
+        elif func(*args,)>10000 and func(*args)<=100000:
+            def mk1k(*args,):
                 y=100000
                 x=func(*args)
                 func(*args)
                 return print(f'{round(((x/y)*100), 5)}% from 100 000')
             return mk1k()
-        elif func(*args)>100000 and func(*args)<=1000000:
-            def mk1k(*args):
+        
+        elif func(*args,)>100000 and func(*args)<=1000000:
+            def mk1k(*args,):
                 y=1000000
                 x=func(*args)
                 func(*args)
                 return print(f'{round(((x/y)*100), 5)}% from 1 000 000')
             return mk1k()
-        elif func(*args)>1000000 and func(*args)<=10000000:
-            def mk1k(*args):
+        
+        elif func(*args,)>1000000 and func(*args)<=10000000:
+            def mk1k(*args,):
                 y=10000000
                 x=func(*args)
                 func(*args)
                 return print(f'{round(((x/y)*100), 5)}% from 10 000 000')
             return mk1k()
-        elif func(*args)>10000000 and func(*args)<=100000000:
-            def mk1k(*args):
+        
+        elif func(*args,)>10000000 and func(*args)<=100000000:
+            def mk1k(*args,):
                 y=100000000
                 x=func(*args)
                 func(*args)
                 return print(f'{round(((x/y)*100), 5)}% from 100 000 000')
             return mk1k()
-        elif func(*args)>100000000 and func(*args)<=1000000000:
-            def mk1k(*args):
+        
+        elif func(*args,)>100000000 and func(*args)<=1000000000:
+            def mk1k(*args,):
                 y=1000000000
                 x=func(*args)
                 func(*args)
                 return print(f'{round(((x/y)*100), 5)}% from 1 000 000 000')
-        elif func(*args)>1000000000 and func(*args)<=10000000000:
-            def mk1k(*args):
+            
+        elif func(*args,)>1000000000 and func(*args)<=10000000000:
+            def mk1k(*args,):
                 y=10000000000
                 x=func(*args)
                 func(*args)
                 return print(f'{round(((x/y)*100), 5)}% from 10 000 000 000')
             return mk1k()
-        elif func(*args)>10000000000 and func(*args)<=100000000000:
-            def mk1k(*args):
+        
+        elif func(*args,)>10000000000 and func(*args)<=100000000000:
+            def mk1k(*args,):
                 y=100000000000
                 x=func(*args)
                 func(*args)
                 return print(f'{round(((x/y)*100), 5)}% from 100 000 000 000')
             return mk1k()
-        elif func(*args)>100000000000 and func(*args)<=1000000000000:
-            def mk1k(*args):
+        
+        elif func(*args,)>100000000000 and func(*args)<=1000000000000:
+            def mk1k(*args,):
                 y=1000000000000
                 x=func(*args)
                 func(*args)
                 return print(f'{round(((x/y)*100), 5)}% from 1 000 000 000 000')
             return mk1k()
+        
     return ifif
