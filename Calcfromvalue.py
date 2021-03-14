34cc651a54c405d2b0f4d87c7a0f73294dc175cc
gast-andrzej <71961055+gast-andrzej@users.noreply.github.com>
2021-03-14 10:47:23 +0100

    Update Calcfromvalue.py

--- Calcfromvalue.py
+++ Calcfromvalue.py
@@ -4,6 +4,7 @@ How many percent is the value of x in relation to y
 """
 
 def Calcfrom_int(first_parametr,
-                 second_parametr,):
+                 second_parametr,
+                ):
     
     return int(round(((first_parametr/second_parametr)*100), 5))
