3d8b0c449992fdf448525db233423579084232b4
gast-andrzej <71961055+gast-andrzej@users.noreply.github.com>
2021-03-14 10:47:00 +0100

    Update Calcfromvalue.py

--- Calcfromvalue.py
+++ Calcfromvalue.py
@@ -2,5 +2,8 @@
 Makroscale->Useful Function
 How many percent is the value of x in relation to y
 """
-def Calcfrom_int(x,y,):
-    return int(round(((x/y)*100), 5))
+
+def Calcfrom_int(first_parametr,
+                 second_parametr,):
+    
+    return int(round(((first_parametr/second_parametr)*100), 5))
